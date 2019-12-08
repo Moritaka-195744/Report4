98341ef290694611e91357164dd871238b70bdee
e195744 <e195744@ie.u-ryukyu.ac.jp>
2019-12-08 23:48:56 +0900

    step2

--- src/main/Report4.java
+++ src/main/Report4.java
@@ -1,11 +1,13 @@
 public class Report4 {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println("NullPointException が発生しました。");
-            System.out.println(e.getMessage());
-        }
+        //try{
+            //String str = null;
+            //System.out.println(str.length());
+        //} catch (NullPointerException e){
+            //System.out.println("NullPointException が発生しました。");
+            //System.out.println(e.getMessage());
+        //}
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
